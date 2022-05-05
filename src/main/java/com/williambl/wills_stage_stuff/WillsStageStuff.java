43c3df1f3b69e1d831ecf95c4516a510e1e2d3cf
williambl <william@williambl.com>
2022-05-05 09:21:17 +0100

    Set up will's stage stuff

---
+++ src/main/java/com/williambl/wills_stage_stuff/WillsStageStuff.java
@@ -0,0 +1,14 @@
+package com.williambl.wills_stage_stuff;
+
+import org.quiltmc.loader.api.ModContainer;
+import org.quiltmc.qsl.base.api.entrypoint.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WillsStageStuff implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger(WillsStageStuff.class);
+
+	@Override
+	public void onInitialize(ModContainer mod) {
+	}
+}
